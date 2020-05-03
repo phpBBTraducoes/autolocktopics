ecfd6ae32e0fe5f5554114eac5d28266a5484c3e
eunaumtenhoid <eunaumtenhoid@outlook.com>
2020-05-03 12:31:01 -0300

    UPDATE

--- language/pt_br/acp/info_acp_forums.php
+++ language/pt_br/acp/info_acp_forums.php
@@ -2,7 +2,7 @@
 
 /**
  * Auto-lock Topics Extension for phpBB.
- * Brazilian Portuguese translation by eunaumtenhoid [2017][ver 1.1.2](https://github.com/phpBBTraducoes)
+ * Brazilian Portuguese translation by eunaumtenhoid [2020][ver 1.2.0](https://github.com/phpBBTraducoes)
  * @author Alfredo Ramos <alfredo.ramos@yandex.com>
  * @copyright 2017 Alfredo Ramos
  * @license GPL-2.0-only
@@ -25,17 +25,17 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'ACP_FORUM_AUTO_LOCK_SETTINGS'	=> 'Configurações Auto-trancar',
+	'ACP_FORUM_AUTO_LOCK_SETTINGS'	=> 'Configurações do Auto-trancar',
 	'ACP_ENABLE_AUTO_LOCK'			=> 'Ativar auto-trancar',
-	'ACP_AUTO_LOCK_ANNOUNCEMENTS'	=> 'Auto-trancar anúncios',
+	'ACP_AUTO_LOCK_ANNOUNCEMENTS'	=> 'Auto-trancar anúncio',
 	'ACP_AUTO_LOCK_STICKIES'		=> 'Auto-trancar fixos',
 	'ACP_AUTO_LOCK_POLLS'			=> 'Auto-trancar enquetes',
 	'ACP_AUTO_LOCK_POLLS_EXPLAIN'	=> 'Trancar tópicos com enquetes não votadas baseadas na idade do post em dias.',
 	'ACP_AUTO_LOCK_DAYS'			=> 'Auto-trancar post por idade',
 	'ACP_AUTO_LOCK_DAYS_EXPLAIN'	=> 'Número de dias desde o último post.',
-	'ACP_AUTO_LOCK_FREQ'		=> 'Freqüência do Auto-trancar',
+	'ACP_AUTO_LOCK_FREQ'		=> 'Frequência do Auto-trancar',
 	'ACP_AUTO_LOCK_FREQ_EXPLAIN' => 'Tempo em dias entre os eventos do Auto-trancar.',
 
 	// %s => Forum name
-	'LOG_AUTO_LOCK_TOPIC'	=> '<strong>Tópicos auto-trancados</strong><br />» %s'
+	'LOG_AUTO_LOCK_TOPIC'	=> '<strong>Tópicos trancados automaticamente</strong><br>» %s'
 ]);
